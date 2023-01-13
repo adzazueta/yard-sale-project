323b89eb494029e226ca6c710f0424cadb05f805
Alexis D. Zazueta <alexisd.zazueta@gmail.com>
2023-01-12 20:27:19 -0600

    feat: render products

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const $shoppingCartBtn = document.querySelector(".navbar-shopping-cart");
 const $desktopMenu = document.querySelector(".desktop-menu");
 const $mobileMenu = document.querySelector(".mobile-menu");
 const $shoppingCart = document.querySelector(".product-detail");
+const $cardsContainer = document.querySelector(".cards-container");
 
 function closeShoppingCart() {
     const isShoppingCartOpen = !$shoppingCart.classList.contains("inactive");
@@ -41,3 +42,65 @@ function toggleShoppingCart() {
 $menuEmail.addEventListener("click", toggleDesktopMenu);
 $burgerMenu.addEventListener("click", toggleMobileMenu);
 $shoppingCartBtn.addEventListener("click", toggleShoppingCart);
+
+const productList = [
+    {
+        name: "Bike",
+        price: 120.0,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        name: "Bike",
+        price: 120.0,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        name: "Bike",
+        price: 120.0,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        name: "Bike",
+        price: 120.0,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        name: "Bike",
+        price: 120.0,
+        img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+];
+
+function renderProducts(product) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.img);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoData = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = `$${product.price}`;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    const productAddToCart = document.createElement("figure");
+
+    const productAddToCartImg = document.createElement("img");
+    productAddToCartImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productAddToCart.append(productAddToCartImg);
+    productInfoData.append(productPrice, productName);
+    productInfo.append(productInfoData, productAddToCart);
+    productCard.append(productImg, productInfo);
+    $cardsContainer.append(productCard);
+}
+
+productList.forEach(function (product) {
+    renderProducts(product);
+});
